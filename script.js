bb717cf54dde32598e9540367437fd4f641e1d3f
Heshan93 <69497422+Heshan93@users.noreply.github.com>
2021-12-28 10:44:23 +0530

    shange the file

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value; // get the value from principal
 
-    var rate = document.getElementById("rate").value;
+    var rate = document.getElementById("rate").value; // get the value from rate
 
-    var years = document.getElementById("years").value;
+    var years = document.getElementById("years").value;  //get the value from years
 
 
-    var year = new Date().getFullYear()+parseInt(years) - 1;
+    var year = new Date().getFullYear()+parseInt(years) - 1;  // get the cuurent date
 
    
    
-   var amount = principal * years * rate / 100;
+   var amount = principal * years * rate / 100;  // interest rate logc
 
 
-
+    // output the result
    document.getElementById("result").innerHTML=  "If you deposit " +  "<span class='highlight'>" +principal+  "</span>" + ",\<br\>at an interest rate of " +  "<span class='highlight'>"   +rate+  "%" + "</span>" +  "\<br\>You will receive an amount of " + "<span class='highlight'>"   +amount+  "</span>" +  ",\<br\>in the year "  + "<span class='highlight'>"  +year+ "</span>" +   "\<br\>";
 
 
-
-
+    // input validation for principal
+   if (principal <= 0) {
+    alert('Please enter positve principal amount.');
+    document.getElementById('principal').focus();
+    return;
+  }
 
 
     }
 
-//https://github.com/kirankumarsripati/vftvk-Simple-Interest-Calculator/blob/master/script.js
+
         
 
-
+// display the updated numbers from the range slider
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
